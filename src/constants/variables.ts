f9b29930e84378bebab13e12ab95cbd8e6d91a06
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-06-27 19:26:00 +0700

    chore(const-vars): added GITHUB_GRAPHQL_ENDPOINT & PUBLIC_GITHUB_API_KEY

---
+++ src/constants/variables.ts
@@ -0,0 +1,5 @@
+export const GITHUB_GRAPHQL_ENDPOINT: string =
+  process.env.REACT_APP_GITHUB_GRAPHQL_ENDPOINT!;
+
+export const PUBLIC_GITHUB_API_KEY: string =
+  process.env.REACT_APP_PUBLIC_GITHUB_API_KEY!;
