edafd5e203aa1de056de37d41e5bfbe812491cd6
Thiti-Dev <thiti.mwk.main@gmail.com>
2022-06-27 23:07:14 +0700

    impl(gql-cont): GET_TOTAL_CONTRIBUTIONS_OF_USER

--- src/constants/gql/GET_TOTAL_CONTRIBUTIONS.ts
+++ src/constants/gql/GET_TOTAL_CONTRIBUTIONS.ts
@@ -1,17 +1,21 @@
 import { gql } from "@apollo/client";
 
-export const GET_TOTAL_CONTRIBUTIONS = gql`
-  query GetTotalContributions($username: String!) {
+export const GET_TOTAL_CONTRIBUTIONS_OF_USER = gql`
+  query GetTotalContributions(
+    $username: String!
+    $from: DateTime!
+    $to: DateTime!
+  ) {
     user(login: $username) {
-      contributionsCollection {
+      contributionsCollection(from: $from, to: $to) {
         contributionCalendar {
           totalContributions
           weeks {
             contributionDays {
+              date
+              weekday
               contributionLevel
               contributionCount
-              weekday
-              date
             }
           }
         }
